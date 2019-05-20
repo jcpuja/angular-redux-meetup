744c79c62143ff37df6671672cb077dc8731634e
Jean-Christophe Puja <jpu@bluesky-it.ch>
2019-05-20 07:58:20 +0200

    Set up component for start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>Redux Test</h1>'
 })
 export class AppComponent {
-  title = 'angular-redux-meetup';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {DevToolsExtension, NgReduxModule} from '@angular-redux/store';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgReduxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private devTools: DevToolsExtension) {
+  }
+
+
+  private getDevToolsEnhancer() {
+    return this.devTools.isEnabled() ?
+      [this.devTools.enhancer()] :
+      [];
+  }
+
+}
