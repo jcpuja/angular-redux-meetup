669da9272e2a2fe9f480f703d1aa323acf4d91c6
Jean-Christophe Puja <jpu@bluesky-it.ch>
2019-05-20 08:01:30 +0200

    Template string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Redux Test</h1>'
+  template: `<h1>Redux Test</h1>`
 })
 export class AppComponent {
 }
